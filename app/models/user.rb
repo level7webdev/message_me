66e192adc5c9678e270e5498efec82bfd06e4ea2
level7webdev <6236517+level7webdev@users.noreply.github.com>
2020-10-27 10:27:56 +0000

    Adds message resource and seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: { case_sensetive: false }, length: { minimum: 3, maximum: 15 }
   has_secure_password
+
+  has_many :messages
   
 end
